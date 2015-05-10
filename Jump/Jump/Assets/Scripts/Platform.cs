6841c722b8b9d4e1058445ea7d54272acc664a31
M. Walker <masimba@blueyonder.co.uk>
2015-05-10 17:44:31 +0100

    Working on moving platforms

--- Jump/Jump/Assets/Scripts/Platform.cs
+++ Jump/Jump/Assets/Scripts/Platform.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Platform : MonoBehaviour 
 {
 	public delegate void PlayerTrigger();
-	
+    private const float EXIT_SPEED = 3.0f;
 	public static event PlayerTrigger OnPlayerJoinedPlatform;
 	public static event PlayerTrigger OnPlayerLeftPlatform;
 
@@ -13,7 +13,9 @@ public class Platform : MonoBehaviour
 	private Shake mShake;
 	private float mTimeSinceActive = 0.0f;
 	private bool mHasBeenActivated = false;
-
+    private int mPlatformDirection = 0;
+    private bool mWillMove; //Will the platform change which direction it's moving in
+    
 	public float DestroyTime { set; get; }
 
 	void OnEnable()
@@ -40,7 +42,7 @@ public class Platform : MonoBehaviour
 				}
 								
 				mHasBeenActivated = true;
-				if( mShake != null )
+				if( mShake != null && !mWillMove)
 				{
 					mShake.enabled = true;
 				}
@@ -65,14 +67,36 @@ public class Platform : MonoBehaviour
 
 	void Update()
 	{
+        
 		if( mHasBeenActivated )
 		{
 			mTimeSinceActive += Time.deltaTime;
-			if( mTimeSinceActive > DestroyTime )
+			if( mTimeSinceActive > DestroyTime && !mWillMove)
 			{
 				gameObject.SetActive( false );
 				IsAlive = false;
+           
 			}
+            else if ( mTimeSinceActive > DestroyTime && mWillMove)
+            {
+                transform.Translate(mPlatformDirection * EXIT_SPEED * Time.deltaTime, 0, 0);
+
+                //If this object's x & y are off the cameras view
+                if(Camera.main.WorldToViewportPoint(transform.position).x < 0 || Camera.main.WorldToViewportPoint(transform.position).x > 1)
+                {//set it to inactive
+                    gameObject.SetActive( false );
+                    IsAlive = false;
+                }
+            }
 		}
+        
 	}
+
+    public void setType(int randomNumber)
+    {
+        mWillMove = (randomNumber < 10); //True if less than 10, false if greater
+        while (mPlatformDirection == 0) //randomise it's direction
+            mPlatformDirection = Random.Range(-1, 1);
+    }
+
 }
