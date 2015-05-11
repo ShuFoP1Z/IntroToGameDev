253981992d0959b36b0dff9592d733d1bbe0784f
M. Walker <masimba@blueyonder.co.uk>
2015-05-11 09:44:19 +0100

    Added logo & rotating platform

--- Jump/Jump/Assets/Scripts/Platform.cs
+++ Jump/Jump/Assets/Scripts/Platform.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Platform : MonoBehaviour 
 {
 	public delegate void PlayerTrigger();
-    private const float EXIT_SPEED = 3.0f;
+    private const float EXIT_SPEED = 4.2f;
+    private const float ROTATE_SPEED = 3.5f;
 	public static event PlayerTrigger OnPlayerJoinedPlatform;
 	public static event PlayerTrigger OnPlayerLeftPlatform;
 
@@ -15,7 +16,8 @@ public class Platform : MonoBehaviour
 	private bool mHasBeenActivated = false;
     private int mPlatformDirection = 0;
     private bool mWillMove; //Will the platform change which direction it's moving in
-    
+    private bool mWillRotate;
+    private bool playerLeft = false;
 	public float DestroyTime { set; get; }
 
 	void OnEnable()
@@ -42,7 +44,7 @@ public class Platform : MonoBehaviour
 				}
 								
 				mHasBeenActivated = true;
-				if( mShake != null && !mWillMove)
+				if( mShake != null && !mWillRotate)
 				{
 					mShake.enabled = true;
 				}
@@ -71,13 +73,13 @@ public class Platform : MonoBehaviour
 		if( mHasBeenActivated )
 		{
 			mTimeSinceActive += Time.deltaTime;
-			if( mTimeSinceActive > DestroyTime && !mWillMove)
+			if( mTimeSinceActive > DestroyTime && !mWillMove && !mWillRotate)
 			{
 				gameObject.SetActive( false );
 				IsAlive = false;
            
 			}
-            else if ( mTimeSinceActive > DestroyTime && mWillMove)
+            else if ( mTimeSinceActive > DestroyTime && mWillMove && !mWillRotate)
             {
                 transform.Translate(mPlatformDirection * EXIT_SPEED * Time.deltaTime, 0, 0);
 
@@ -87,6 +89,16 @@ public class Platform : MonoBehaviour
                     gameObject.SetActive( false );
                     IsAlive = false;
                 }
+            }else if(mTimeSinceActive > DestroyTime && mWillRotate && !mWillMove)
+            {
+                transform.Rotate(0, 0, mPlatformDirection * EXIT_SPEED * Time.deltaTime);
+                Debug.Log(playerLeft);
+                if (Camera.main.WorldToViewportPoint(transform.position).y > 1 || Camera.main.WorldToViewportPoint(transform.position).y < 0)
+                {
+                    gameObject.SetActive(false);
+                    IsAlive = false;
+                    transform.rotation.SetAxisAngle(new Vector3(0, 0, 1), 0);
+                }
             }
 		}
         
@@ -94,7 +106,9 @@ public class Platform : MonoBehaviour
 
     public void setType(int randomNumber)
     {
-        mWillMove = (randomNumber < 10); //True if less than 10, false if greater
+        mWillMove = (randomNumber < 30 && randomNumber > 20); //True if less than 10, false if greater
+        mWillRotate = (randomNumber < 20);
+
         while (mPlatformDirection == 0) //randomise it's direction
             mPlatformDirection = Random.Range(-1, 1);
     }
